d33f59e247ef005e05ac1c862ce7d80f61124d48
Redhun <redhuns123@gmail.com>
2022-11-29 23:40:13 +0530

    version2

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'hello my name261 :',
+              'hello my namel261 :',
             ),
             Text(
               '$_counter',
